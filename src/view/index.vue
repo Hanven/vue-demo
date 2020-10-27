872c303d2ffed96fe4c979a75f6174204078d8a3
Hanven <1141854584@qq.com>
2020-10-27 17:03:35 +0800

    修改首页

---
+++ src/view/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        i am index
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            msg:'我是首页'
+        }
+    },
+    methods:{
+
+    },
+    mounted(){
+
+    }
+}
+</script>
